cf00dbcdf81a81a865025f86a39c979b5945e3f7
DSegura <absol.1994@gmail.com>
2024-03-01 15:33:15 -0600

    Delete controller.js

--- controllers/controller.js
+++
@@ -1,8 +0,0 @@
-// Handles logic for user-related API endpoints (e.g., sign-up, login, profile retrieval).
-// Create as many files as needed  remember to keep it encapsulated
-// I would sayt that this is in fact required at minimun if you want to create logic for your program
-
-
-exports.helloWorld = (req, res) => {
-    res.send('Hello World!');
-  };
