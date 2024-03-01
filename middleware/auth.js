beb93266e9e01ad0a9636c6592361f50e5522fb9
DSegura <absol.1994@gmail.com>
2024-03-01 15:33:23 -0600

    Delete auth.js

--- middleware/auth.js
+++
@@ -1,6 +0,0 @@
-// Auth.Js Defines an authentication middleware function that checks for a valid authorization token before proceeding with protected routes.
-// This is just an example of middleware this is not required if you dont want to use it, 
-// to use it you need to import it on any of the index.js files
-// You can also create other middleware to validate inputs or modify the inputs
-// This is not always required for personal projects unless you want to add more compexity or more features
-// or you need to perform certain validations or changes to your inputs
