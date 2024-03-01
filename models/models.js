0de097a7155bdb5243625635af42a1fcf201dabb
DSegura <absol.1994@gmail.com>
2024-03-01 15:33:30 -0600

    Delete models.js

--- models/models.js
+++
@@ -1,4 +0,0 @@
-// Defines the db  schema or other ORM structure for the User model.
-// Basically here you define any object that you are going to use here
-// ex: db.getUser(userId) SO you create the object db here and the query in SQL to retrieve that info.
-// If you are not requiring to use any models or objects you dont actually need this file
